211fbefc2961e89d74525d03ae7a78c09b82b3b4
Ravibhai Sakaliya <r.b.sakaliya@gmail.com>
2026-05-17 04:09:47 -0400

    dfhdhf

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,11 +46,11 @@ app.use("/api/report", reportRoutes);
 
 //  Serve static files AFTER API routes
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // Fallback only for non-API routes
   app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 }
 
